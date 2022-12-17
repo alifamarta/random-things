ecca077685e20d2e884122ca7df047701c99839a
Alif Amarta <alifamarta23@gmail.com>
2022-12-17 21:41:03 +0700

    Add files via upload

---
+++ C++/Iseng/SquareFill.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    for(int row = 1;row <= 10; ++row){
+        for(int column = 1;column<=20;++column){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
