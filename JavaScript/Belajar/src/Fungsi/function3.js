cca767eb0da1ea7ece475e0bcb125db0ca988cc1
Alif Amarta <alifamarta23@gmail.com>
2023-01-26 21:56:21 +0700

    Add files via upload

---
+++ JavaScript/Belajar/src/Fungsi/function3.js
@@ -0,0 +1,11 @@
+function tambah(a,b){
+    return a+b;
+}
+
+function kali(a,b){
+    return a*b;
+}
+
+var hasil = kali(tambah (2,4), tambah(4,3));
+
+console.log(hasil)
---
+++ JavaScript/Belajar/src/Fungsi/function4.js
@@ -0,0 +1,6 @@
+function list(){
+    return arguments;
+}
+
+var list = list(5,9,10,87,46)
+console.log(list);
